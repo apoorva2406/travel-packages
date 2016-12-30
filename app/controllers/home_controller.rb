a9077c7851a0bb1b91273cc9ff3609471ed700be
Kapil <kapil.yuvasoft@gmail.com>
2016-12-30 18:05:15 +0530

    Fix home locations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
 		@properites = Property.varified_property.add_to_home_property
-		@properites = Property.all if @properites.blank?
+		#@properites = Property.all if @properites.blank?
 	end
 end
